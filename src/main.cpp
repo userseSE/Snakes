a1b90299f5880bd1f52568b01878571cc4baf563
gugu <yuyao_wang@foxmail.com>
2023-04-26 11:13:06 +0800

    add flecs submodule

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,23 @@
-void test(double p1, double p2, double &p3)
-{
-    p3 = p1 + p2;
-}
+#include <flecs.h>
+
+struct map{
+    double x;
+    double y;
+    double z;
+};
 
 int main()
 {
-    return;
+    flecs::world ecs;
+    
+   auto system = ecs.system<map>()
+        .kind(flecs::OnSet)
+        .each([](flecs::entity e, map & m) {
+            printf("x: %f, y: %f, z: %f", m.x, m.y, m.z);
+        });
+    ecs.entity()
+        .set<map>({1, 2, 3});
+    ecs.progress();
+    system.run();
+    return 0;
 }
