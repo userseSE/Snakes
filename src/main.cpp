8c7af8dfdd2c31870d5574e124b50be24ac7dbe1
jinkela <jinkela@sdyg.com>
2023-04-25 21:40:31 -0600

    update raylib setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,44 @@
-#include <flecs.h>
 
-struct map{
-    double x;
-    double y;
-    double z;
-};
+#include <vector>
 
-int main()
+
+#include "flecs.h"
+#include "raylib-cpp.hpp"
+
+
+int main(int argc, char* argv[])
 {
-    flecs::world ecs;
+
     
-   auto system = ecs.system<map>()
-        .kind(flecs::OnSet)
-        .each([](flecs::entity e, map & m) {
-            printf("x: %f, y: %f, z: %f", m.x, m.y, m.z);
-        });
-    ecs.entity()
-        .set<map>({1, 2, 3});
-    ecs.progress();
-    system.run();
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    int screenWidth = 800;
+    int screenHeight = 450;
+    raylib::Color textColor = raylib::Color::LightGray();
+    raylib::Window window(screenWidth, screenHeight, "raylib [core] example - basic window");
+
+    SetTargetFPS(60);
+    //--------------------------------------------------------------------------------------
+
+    // Main game loop
+    while (!window.ShouldClose()) {   // Detect window close button or ESC key
+        // Update
+        //----------------------------------------------------------------------------------
+        // Update your variables here
+        //----------------------------------------------------------------------------------
+
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+        {
+            window.ClearBackground(RAYWHITE);
+            textColor.DrawText("Congrats! You created your first window!", 190, 200, 20);
+        }
+        EndDrawing();
+        //----------------------------------------------------------------------------------
+    }
+
+    return 0;
+
     return 0;
 }
